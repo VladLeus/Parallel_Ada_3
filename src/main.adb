360d784694f334e9df8d3a2d9790b2157615dcad
VladLeus <57189571+VladLeus@users.noreply.github.com>
2024-04-12 12:10:47 +0300

    Update main.adb
    )

--- src/main.adb
+++ src/main.adb
@@ -11,24 +11,37 @@ procedure Main is
    Space_Available: Counting_Semaphore (Capacity, Default_Ceiling);
    Items_Avalable: Counting_Semaphore (0, Default_Ceiling);
 
-   task Producer;
-   task Consumer;
+    task type Producer is
+          entry Start(Items_To_Produce : in Integer);
+    end Producer;
+
+      task type Consumer is
+          entry Start(Items_To_Consume : in Integer);
+      end Consumer;
 
    task body Producer is
+       Items_To_Produce : Integer;
    begin
-      for I in 0 .. Capacity loop
+        accept Start (Items_To_Produce : in Integer) do
+                Producer.Items_To_Produce := Items_To_Produce;
+        end Start;
+      for I in 0 .. Items_To_Produce loop
          Space_Available.Seize;
          Items_Storage.Append(I);
          Put_Line("Producer added item:" & I'Img);
          Items_Avalable.Release;
 
-         --delay 1.5;
+         --delay 2.5;
       end loop;
    end Producer;
 
    task body Consumer is
+       Items_To_Consume : Integer;
    begin
-      for I in 0 .. Capacity loop
+       accept Start (Items_To_Consume : in Integer) do
+                Consumer.Items_To_Consume := Items_To_Consume;
+        end Start;
+      for I in 0 .. Items_To_Consume loop
          Items_Avalable.Seize;
 
          declare
@@ -40,15 +53,27 @@ procedure Main is
          Items_Storage.Delete_First;
 
          Space_Available.Release;
-         --delay 2.5;
+         --delay 1.5;
       end loop;
    end Consumer;
+   
+   type Producers_Array_Type is array (Integer range <>) of Producer;
+    type Consumers_Array_Type is array (Integer range <>) of Consumer;
+    Producers : Producers_Array_Type (1 .. 5);
+    Consumers : Consumers_Array_Type (1 .. 3);
 
-   begin
-      null;
-   end Storage;
+begin
+  for I in Producers'Range loop
+        Producers(I).Start(10);
+    end loop;
+    
+    for J in Consumers'Range loop
+            Consumers(J).Start(10);
+    end loop;
+end Storage;
+
+
 
 begin
    Storage(10);
 end Main;
-
